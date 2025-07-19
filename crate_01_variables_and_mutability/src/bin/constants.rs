89b9832a80b84550ab859523f3975371259c61d9
usman1515 <usmansiddique1515@gmail.com>
2025-07-19 23:51:50 +0200

    crate 01 lesson 05

---
+++ crate_01_variables_and_mutability/src/bin/constants.rs
@@ -0,0 +1,10 @@
+const SALES_TAX: f32 = 6.789;
+
+fn main() {
+    println!("========== constants ==========");
+
+    // constants
+    const INCOME_TAX: f32 = 3.45;
+    println!("Income tax rate:      {}", INCOME_TAX);
+    println!("Sales tax rate:       {}", SALES_TAX);
+}
